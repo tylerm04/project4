fa4f1260fdd8aec9b3e406221e9cfd3addaa4504
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2021-01-29 15:14:53 +0000

    I made the methods and started working on calling them

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   Scanner scan = new Scanner(System.in);
+   
+   System.out.println("What is your name?");
+    String name = scan.next();
+   
+   y
+
+   static boolean weather(boolean rain) 
+  {
+    if(rain) 
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  static String forecast(String name) 
+  {
+    String forecast = "It is raining out side " + name
+    return forecast;
+  }
   }
 }
