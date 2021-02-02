e9ab5a23cc529fee518dc8a23677684d78891a44
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2021-02-02 13:10:56 +0000

    added more to the boolean method and changed the string method.

--- Main.java
+++ Main.java
@@ -10,24 +10,28 @@ class Main
    
    System.out.println("Is it raining?");
     Boolean rain = scan.nextBoolean();
-
+   
+   System.out.println(weather(rain));
    System.out.println(forecast(name));
+
   }
    static boolean weather(boolean rain) 
   {
-    if(rain) 
+    if(rain)  
     {
+      System.out.println("Get inside!");
       return true;
     }
     else
     {
+      System.out.println("Continue on!");
       return false;
     }
   }
 
   static String forecast(String name) 
   {
-    String forecast = "It is raining out side " + name;
+    String forecast = "Have a great day " + name + "!";
     return forecast;
   }
   
