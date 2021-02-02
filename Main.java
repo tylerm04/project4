55c3d68219cfa5ba969f299cb18b46723d841f06
Tyler Mcdowell <tyler.mcdowell@patelhs.org>
2021-02-02 13:16:23 +0000

    added comments

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; //scanner added 
 class Main 
 {
   public static void main(String[] args) 
@@ -6,33 +6,33 @@ class Main
    Scanner scan = new Scanner(System.in);
    
    System.out.println("What is your name?");
-    String name = scan.next();
+    String name = scan.next();//asking users name
    
-   System.out.println("Is it raining?");
-    Boolean rain = scan.nextBoolean();
+   System.out.println("Is it raining? True/False");
+    Boolean rain = scan.nextBoolean(); //asking user if it's raining
    
    System.out.println(weather(rain));
-   System.out.println(forecast(name));
+   System.out.println(day(name));//calling the methods
 
   }
-   static boolean weather(boolean rain) 
+   static boolean weather(boolean rain) //boolean method
   {
     if(rain)  
     {
-      System.out.println("Get inside!");
+      System.out.println("Get inside!");//prints if its raining
       return true;
     }
     else
     {
-      System.out.println("Continue on!");
+      System.out.println("Continue on!");//prints if its not raining
       return false;
     }
   }
 
-  static String forecast(String name) 
+  static String day(String name) //string method
   {
     String forecast = "Have a great day " + name + "!";
-    return forecast;
+    return forecast;//prints at the end
   }
   
 }
